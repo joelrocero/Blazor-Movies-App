4af677371ffe2c6d44dd6e0092ad7b1686a7427a
Joel.Rocero <joel.rocero@outlook.com>
2023-02-17 14:42:31 +0800

    Add project files.

---
+++ BlazorMovies/Client/Services.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMovies.Client
+{
+    public class SingletonService
+    {
+        public int Value { get; set; }
+    }
+
+    public class TransientService
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ BlazorMovies/Server/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMovies.Server
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoviesActors>().HasKey(x => new { x.MovieId, x.PersonId });
+            modelBuilder.Entity<MoviesGenres>().HasKey(x => new { x.MovieId, x.GenreId });
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<MoviesActors> MoviesActors{ get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+        public DbSet<MovieRating> MovieRatings { get; set; }
+
+    }
+}
